5ea6150578df0938aacfa81948954dc62097cc0f
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-23 11:49:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ class App{
         this.setEnvironment();
         
         this.reticle = new THREE.Mesh(
-            new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
+            //new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
+
+            new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( Math.PI / 2 ),
             new THREE.MeshBasicMaterial()
         );
         
