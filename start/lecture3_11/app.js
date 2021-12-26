3e5329b58b8ba41be032c2530603cf65c7c53e00
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-26 21:02:29 +0500

    office chair2

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -104,8 +104,9 @@ class App{
 			// called when loading has errors
 			function ( error ) {
 
-				console.log( 'An error happened' );
+				//console.log( 'An error happened' );
 
+                console.log( console.error() );
 			}
 		);
         
