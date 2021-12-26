a4476a0f00e80297f25340f23ae9d4cc1dcdcde2
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-26 22:32:57 +0500

    Update app.js

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -154,7 +154,7 @@ class App{
             if (!self.knight.object.visible){
                 self.knight.object.visible = true;
                 //self.knight.object.position.set( 0, -0.3, -0.5 ).add( ev.position );
-                self.knight.object.position.set( 0, 0, 0 ).add( ev.position );
+                self.knight.object.position.set( 1, -0.3, -0.5 ).add( ev.position );
 
                 self.scene.add( self.knight.object ); 
             }
