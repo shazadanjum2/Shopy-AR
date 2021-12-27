43cab4b5552f8332b89f00511e2ec58117d8845a
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-27 14:25:19 +0500

    Update app.js

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -16,7 +16,7 @@ class App{
         this.clock = new THREE.Clock();
         
 		//this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.01, 20 );
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );//0.01
+        this.camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 20 );//0.01
 
 		this.scene = new THREE.Scene();
         
@@ -155,7 +155,7 @@ class App{
             if (!self.knight.object.visible){
                 self.knight.object.visible = true;
                 //self.knight.object.position.set( 0, -0.3, -0.5 ).add( ev.position );
-                self.knight.object.position.set( 0, -0.3, -4 ).add( ev.position );
+                self.knight.object.position.set( 0, -0.3, -5 ).add( ev.position );
 
                 self.scene.add( self.knight.object ); 
             }
