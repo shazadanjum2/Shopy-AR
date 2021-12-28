0df75933721d21c3a2e6e4f3260d991d1eba8351
shazadanjum2 <75418972+shazadanjum2@users.noreply.github.com>
2021-12-28 14:38:19 +0500

    Update app.js

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -115,7 +115,7 @@ class App{
 				
 				//self.knight.action = 'Dance';
 				//const scale = 0.003;
-                const scale = 0.3;
+                const scale = 0.03;
 				self.knight.object.scale.set(scale, scale, scale); 
 				
                 self.loadingBar.visible = false;
